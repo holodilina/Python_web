047bd73edcf9db920b0e71be29d5a1a1b12d5720
holodilina <112629404+holodilina@users.noreply.github.com>
2023-09-25 17:14:00 +0300

    Update test1_rest_sem.py

--- test1_rest_sem.py
+++ test1_rest_sem.py
@@ -4,4 +4,6 @@ wsdl = 'http://speller.yandex.net/services/spellservice?WSDL'
 setting = Settings(strict=False)
 
 client = Client(wsdl=wsdl, settings=settings)
-print(client.service.сheckText('брыкэ'))
+print(client.service.сheckText('брыкэ')[0]['s'])
+
+
