1669ede9ad2134c90fff124011e96da1888a0dbf
holodilina <112629404+holodilina@users.noreply.github.com>
2023-09-26 00:33:01 +0300

    Update test3_main.py

--- test3_main.py
+++ test3_main.py
@@ -6,10 +6,11 @@ with open("testdata.yaml") as f:
 site = Site(testdata["address"])
 
 def test_step1():
-  x_selector1 = """ """
+  x_selector1 = """//*[@id="login"]/div[1]/label/input"""
   input1 = site.find_element("xpath", x_selector1)
   input1.send_keys("test")
-  x_selector2 = """ """
+  x_selector2 = """//*[@id="login"]/div[2]/label/input"""
   input2 = site.find_element("xpath", x_selector2)
   input2.send_keys("test")
 
+test_step1()
