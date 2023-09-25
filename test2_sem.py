8bdb4597797557be028afa32e6ce67e1b5cca776
holodilina <112629404+holodilina@users.noreply.github.com>
2023-09-25 21:46:39 +0300

    Update test2_sem.py

--- test2_sem.py
+++ test2_sem.py
@@ -6,14 +6,14 @@ with open("config2_sem.yaml") as f:
   data = yaml.safe_load(f)
 
 def login():
-  response  = requests.post("https://test-stand.gb.ru/gateway/login", data={'username': data["login"], 'password': data[password]})
+  response  = requests.post(data[url_login], data={'username': data["login"], 'password': data[password]})
   print(response.status_code)
   if response.status_code == 200:
       return response.json()["token"]
 
 def get(token):
   print(token)
-  response = requests.get("https://test-stand.gb.ru/api/posts", headers={"X-Auth-Token": token}), params={"owner": "NOTME"}}
+  resource = requests.get(data[url_posts], headers={"X-Auth-Token": token}), params={"owner": "NOTME"}}
   print(resource.JSON())
 
 print(get(login()))
