608a835b5da591a13e4be581a4988caa0f27bccc
holodilina <112629404+holodilina@users.noreply.github.com>
2023-09-25 21:15:31 +0300

    Create test2_sem.py

---
+++ test2_sem.py
@@ -0,0 +1,6 @@
+import requests
+
+response  = requests.post('https://test-stand.gb.ru/gateway/login', data ={'username: "GB202306042a214"', 'password: "e778a3dca7"'})
+
+print(response.status_code)
+print(response)
