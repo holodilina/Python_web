4a4db32e1c1314f567089ea85a66cc795d4b4d25
holodilina <112629404+holodilina@users.noreply.github.com>
2023-09-25 17:34:43 +0300

    Update test2_soap_sem.py

--- test2_soap_sem.py
+++ test2_soap_sem.py
@@ -1,8 +1,8 @@
 from test1_soap_sem import check_text
 import pytest
 
-def test_step1():
-  assert 'фыр' in check_text('брыкэ'), "что-то не так"
+def test_step1(good, bad):
+  assert good in check_text(bad), "что-то не так"
 
 
   
