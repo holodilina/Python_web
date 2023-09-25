d77b0de32c288a10f4857e52b6e748d564ac92c7
holodilina <112629404+holodilina@users.noreply.github.com>
2023-09-25 21:36:47 +0300

    Update test2_sem.py

--- test2_sem.py
+++ test2_sem.py
@@ -1,9 +1,14 @@
 import requests
 
 def login():
-  response  = requests.post('https://test-stand.gb.ru/gateway/login', data={'username: "GB2022111948e18"', 'password: "db4297e3bc"'})
-  
-  if response.status_code == 200
+  response  = requests.post("https://test-stand.gb.ru/gateway/login", data={'username': "User_1_Login"', 'password: "6f831d56c2"})
+  print(response.status_code)
+  if response.status_code == 200:
       return response.json()["token"]
 
-print(login())
+def get(token):
+  print(token)
+  response = requests.get("https://test-stand.gb.ru/api/posts", headers={"X-Auth-Token": token}), params={"owner": "NOTME"}}
+  print(resource.JSON())
+
+print(get(login()))
