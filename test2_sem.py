46736e1584d3fe3202befd09dfceacb5ce4c8cb4
holodilina <112629404+holodilina@users.noreply.github.com>
2023-09-25 21:24:15 +0300

    Update test2_sem.py

--- test2_sem.py
+++ test2_sem.py
@@ -3,5 +3,7 @@ import requests
 def login():
   response  = requests.post('https://test-stand.gb.ru/gateway/login', data={'username: "GB2022111948e18"', 'password: "db4297e3bc"'})
   
-  print(response.status_code)
-  print(response.json()["token"])
+  if response.status_code == 200
+      return response.json()["token"]
+
+print(login())
