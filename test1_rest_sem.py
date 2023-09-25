8904be9ab316a84b5b3cf47a62f97f3c10639b0d
holodilina <112629404+holodilina@users.noreply.github.com>
2023-09-25 17:11:23 +0300

    Update test1_rest_sem.py

--- test1_rest_sem.py
+++ test1_rest_sem.py
@@ -1,1 +1,7 @@
+from zeep import Client, Settings
 
+wsdl = 'http://speller.yandex.net/services/spellservice?WSDL'
+setting = Settings(strict=False)
+
+client = Client(wsdl=wsdl, settings=settings)
+print(client.service.сheckText('брыкэ'))
