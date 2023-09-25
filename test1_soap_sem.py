1e8a565707c53162c3839ae278d97c9b8fc6c62c
holodilina <112629404+holodilina@users.noreply.github.com>
2023-09-25 17:17:18 +0300

    Update test1_soap_sem.py

--- test1_soap_sem.py
+++ test1_soap_sem.py
@@ -4,6 +4,8 @@ wsdl = 'http://speller.yandex.net/services/spellservice?WSDL'
 setting = Settings(strict=False)
 
 client = Client(wsdl=wsdl, settings=settings)
-print(client.service.сheckText('брыкэ')[0]['s'])
 
-
+def check_text(word):
+  return client.service.сheckText(word)[0]['s']
+  
+print check_text('брыкэ')
