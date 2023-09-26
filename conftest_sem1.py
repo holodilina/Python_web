2a0f50a3f75bcaed6aaf5d1f9091b113fcf8cbee
holodilina <112629404+holodilina@users.noreply.github.com>
2023-09-26 14:46:11 +0300

    Update conftest_sem1.py

--- conftest_sem1.py
+++ conftest_sem1.py
@@ -2,8 +2,22 @@ import pytest
 import yaml
 
 @pytest.fuxture()
-def selectors()
-    return "//*[@id="login"]/div[1]/label/input"
+def selector_login():
+    return """//*[@id="login"]/div[1]/label/input"""
+
+@pytest.fuxture()
+def selector_pass():
+    return """//*[@id="login"]/div[2]/label/input"""
+
+@pytest.fuxture()
+def selector_btn_submit():
+    return "button"
+
+@pytest.fuxture()
+def selector_err_banner():
+    return """//*[@id="app]/main/div/div[2]/h2"""
+
+
 
 
 
