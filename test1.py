fee16485fcdbfedd4c9c09dad916cbf48e67aaef
holodilina <112629404+holodilina@users.noreply.github.com>
2023-09-27 15:56:43 +0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,26 +1,10 @@
-import yaml
-import module import Site
+from testpage import OperationsHelper
 
-with open("testdata.yaml") as f:
-  testdata = yaml.safe_load(f)
-site = Site(testdata["address"])
-
-def test_step1():
-
-  input1 = site.find_element("xpath", x_selector1)
-  input1.send_keys("test")
+def test_step1(browser):
+  testpage = OperationsHelper(browser)
+  testpage.go_to_site()
+  testpage.enter_login("test")
+  testpage.enter_pass("test")
+  testpage.click_login_button()
+  assert testpage.get_error_text() == "401"
   
-  input2 = site.find_element("xpath", x_selector2)
-  input2.send_keys("test")
-  
-  btn = site.find_element("css", btn_selector)
-  btn.click()
-  
-  err_label = site.find_element("xpath", x_selector3)
-  
-  # print(err_label.text)
-  assert err_label.text == "401"
-  
-button
-
-# test_step1()
