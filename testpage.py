f0c1c9624fb8c4e881b9ea3825a5f8be6934dcc1
holodilina <112629404+holodilina@users.noreply.github.com>
2023-09-27 15:01:15 +0300

    Update testpage.py

--- testpage.py
+++ testpage.py
@@ -7,3 +7,20 @@ class TestSearchLocators:
   LOCATOR_LOGIN_BTN = (By.CSS_SELECTOR, "button")
   LOCATOR_ERROR_FIELD = (By.XPATH, """//*[@id="app]/main/div/div[2]/h2""")
   
+class OperationsHelper(BasePage):
+  def enter_login(self, word):
+    login_field = self.find_element(TestSearchLocators.LOCATOR_LOGIN_FIELD)
+    login_field.clear()
+    login_field.send_keys(word)
+    
+  def enter_pass(self, word):
+    login_field = self.find_element(TestSearchLocators.LOCATOR_PASS_FIELD)
+    login_field.clear()
+    login_field.send_keys(word)
+
+  def click_login_button(self):
+    self.find_element(TestSearchLocators.LOCATOR_LOGIN_BTN).click()
+
+  def get_error_text(self):
+    error_field = self.find_element(TestSearchLocators.LOCATOR_ERROR_FIELD, time=3)
+    return error_field.text
