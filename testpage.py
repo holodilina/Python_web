b57757770798f0bce1b587c83ceb06d40e483f6a
holodilina <112629404+holodilina@users.noreply.github.com>
2023-09-27 14:06:48 +0300

    Update testpage.py

--- testpage.py
+++ testpage.py
@@ -2,8 +2,8 @@ from BaseApp import BasePage
 from selenium.webdriver.common.by import By
 
 class TestSearchLocators:
-  x_selector1 = (By.XPATH, """//*[@id="login"]/div[1]/label/input""")
-  x_selector2 = (By.XPATH, """//*[@id="login"]/div[2]/label/input"""
-  btn_selector = (By.CSS_SELECTOR, "button")
-  x_selector3 = (By.XPATH, """//*[@id="app]/main/div/div[2]/h2""")
+  LOCATOR_LOGIN_FIELD = (By.XPATH, """//*[@id="login"]/div[1]/label/input""")
+  LOCATOR_PASS_FIELD = (By.XPATH, """//*[@id="login"]/div[2]/label/input"""
+  LOCATOR_LOGIN_BTN = (By.CSS_SELECTOR, "button")
+  LOCATOR_ERROR_FIELD = (By.XPATH, """//*[@id="app]/main/div/div[2]/h2""")
   
