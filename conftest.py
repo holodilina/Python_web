5085230692d50789cf43287ee6692175474950e5
holodilina <112629404+holodilina@users.noreply.github.com>
2023-09-27 13:26:07 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,7 +1,5 @@
-import yaml
-import time
+import pytest, yaml
 from selenium import webdriver
-import selenium.webdriver.common.by import By
 from selenium.webdriver.chrom.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
@@ -10,18 +8,17 @@ with open(".testdata.yaml") as f:
   testdata = yaml.sefe_load(f)
   browser = testdata["browser"]
 
-class Site:
-  def __init__(self, address):
-    if browser == "firefox":
-      service = Service(executable_path=GeckoDriverManager().install)
-      options = webdriver.FirefoxOptions()
-      self driver = webdriver.Firefox(service=service, options=options)
-    elif browser == "chrome":
-      service = Service(executable_path=ChromeDriverManager().install)
-      options = webdriver.Chrome(service=service, options=options)
-      self.driver = webdriver.Chrome(service=service, options=options)
+@pytest.fixture(scope="session")
 
-    self.driver.implicitly_wait(3)
-    self.driver.maximize_window()
-    self.driver.get(address)
-    timesleep(testdata["sleep_time"])
+def browser():
+    if browser == "firefox":
+      service = Service(executable_path=GeckoDriverManager().install())
+      options = webdriver.FirefoxOptions()
+      driver = webdriver.Firefox(service=service, options=options)
+    else:
+      service = Service(executable_path=ChromeDriverManager().install())
+      options = webdriver.ChromeOptions()
+      driver = webdriver.Chrome(service=service, options=options)
+    yeld driver
+    driver.quit()
+
